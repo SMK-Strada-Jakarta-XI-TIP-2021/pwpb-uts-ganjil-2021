89757ecbae6819b9fb48aeb9a975d17ed95dbaba
maxdesanta <maxdesanta1@gmail.com>
2021-09-22 22:55:28 +0700

    uts pwpb perubahan pertama

--- jawaban_2.js
+++ jawaban_2.js
@@ -1,9 +1,33 @@
 // Jawaban Soal 2
+// membuat luas
 function luasTanah(lebar, panjang) {
     return panjang * lebar;
 }
 
+// buat harga pajak
 function pajak(lebar, panjang) {
     let luas = luasTanah(lebar, panjang);
-    if()
+    if (luas < 150) {
+        return luas * 100000;
+    } else {
+        return luas * 150000;
+    }
 }
+
+// area tanaman 
+function areaTanaman(lebar, panjang) {
+    let luas = luasTanah(lebar, panjang);
+    return luas / (2 * 2);
+}
+
+// 
+function jmlhBibit(lebar, panjang) {
+    let luas = luasTanah(lebar, panjang);
+    let area = areaTanaman(lebar, panjang);
+    return (luas / (2 * 2)) * area;
+}
+
+console.log(luasTanah(50, 50));
+console.log(pajak(50, 50));
+console.log(areaTanaman(50, 50));
+console.log(jmlhBibit(50,50));
