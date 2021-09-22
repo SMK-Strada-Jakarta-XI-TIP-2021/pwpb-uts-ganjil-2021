e0a97d148da38e2abfad0bcac6acaddb57f7b9b2
sulthanarif <msarizky201@gmail.com>
2021-09-22 22:56:01 +0700

    uts pwpb final fix

--- jawaban_2.js
+++ jawaban_2.js
@@ -1,8 +1,26 @@
 // Jawaban Soal 2
 
-function luas (panjang, lebar){
+function luasLahan (panjang, lebar){
  return panjang * lebar
 }
 function pajak(panjang, lebar){
-    let luas
+    let luas = luasLahan(panjang,lebar);
+    if(luas < 150){
+        return luas * 100000;
+    }
+    else{
+        return luas * 150000;
+    }
 }
+function areaLahan(panjang,lebar){
+    let luas = luasLahan(panjang,lebar);return luas /(2*2);
+}
+function bibitJagung (panjang,lebar){
+    let luas = luasLahan(panjang,lebar);
+    let area = areaLahan(panjang,lebar);
+    return (luas/(2*2)) * area;
+}
+console.log(`luasnya ${luasLahan(12,12)} meter persegi`);
+console.log(`pajak yang harus dibayar adalah Rp${pajak(12,12)}`);
+console.log(`jumlah area tanam ada ${areaLahan(12,12)}`);
+console.log(`jumlah bibit jagung nya ada ${bibitJagung(12,12)}`);
